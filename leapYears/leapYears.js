dd19c22097c8c99f702c710a0850c3149046a02c
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-08 22:54:27 -0400

    Add solution for leapYear

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,5 @@
-var leapYears = function() {
-
+var leapYears = function(yearToTest) {
+    return yearToTest % 4 === 0 && !(yearToTest % 100 === 0 && yearToTest % 400 !== 0) ? true : false;
 }
 
 module.exports = leapYears
