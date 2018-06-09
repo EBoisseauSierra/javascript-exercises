25104eb3eba65625632cdc59d718c395c7bf2dcc
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-08 21:04:38 -0400

    Add solution to reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-var reverseString = function() {
-
+var reverseString = function(stringToReverse) {
+    if (stringToReverse.length > 0) {
+        return stringToReverse.slice(-1) + reverseString(stringToReverse.slice(0,-1));
+    } else {
+        return '';
+    }
 }
 
 module.exports = reverseString
