5c3e3b3cffac19483212bec56ddc53cff1056cbf
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-08 22:45:40 -0400

    Add solution to sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,13 @@
-var sumAll = function() {
-
+var sumAll = function(fromNumber, toNumber) {
+    // ensure both entries are positive integers
+    if ( !(Number.isInteger(fromNumber) && fromNumber >= 0) || !(Number.isInteger(toNumber) && toNumber >= 0)) return 'ERROR';
+    let lowerBoundary = Math.min(fromNumber, toNumber);
+    let higherBoundary = Math.max(fromNumber, toNumber);
+    let sumOfNumberBetweenLowAndHigh = 0;
+    for (let i = lowerBoundary; i <= higherBoundary; i++) {
+        sumOfNumberBetweenLowAndHigh += i;
+    }
+    return sumOfNumberBetweenLowAndHigh;
 }
 
 module.exports = sumAll
