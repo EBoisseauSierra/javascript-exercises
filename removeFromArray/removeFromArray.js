b3babb2ef6f1b0d576c1b830a4d388f4d95e93da
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-08 22:27:34 -0400

    Add solution to removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-var removeFromArray = function() {
-
+var removeFromArray = function(arrayToRemoveFrom, ...elementsToRemove) {
+    for (let elementToRemove of elementsToRemove) {
+        // ensure all instances of elementToRemove are removed
+        while (arrayToRemoveFrom.indexOf(elementToRemove) !== -1) {
+            arrayToRemoveFrom.splice(arrayToRemoveFrom.indexOf(elementToRemove),    1);
+        }
+    }
+    return arrayToRemoveFrom;
 }
 
 module.exports = removeFromArray
