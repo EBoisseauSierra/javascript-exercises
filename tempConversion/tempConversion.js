78ff7efaf853b4feb98739beb7a00c1f581cde62
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-08 23:01:39 -0400

    solution to tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-var ftoc = function() {
-  
+var ftoc = function(tFarenheit) {
+  // round to first decimal
+  return Math.round(((tFarenheit - 32) * 5 / 9) * 10) / 10;
 }
 
-var ctof = function() {
-  
+var ctof = function(tCelsius) {
+  return Math.round(((tCelsius / 5 * 9) + 32) * 10) / 10;
 }
 
 module.exports = {
