625c0a8a545926cabd0741a1a0c7c205f28a2091
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-09 10:54:27 -0400

    Add solution to caesar that fails added test

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,18 @@
-var caesar = function() {
-
+var caesar = function(stringToEncrypt, cipherShift) {
+    let encryptedString = '';
+    for (let index = 0; index < stringToEncrypt.length; index++) {
+        // transform negative shift into positive one, as it otherwise messes with wraping
+        if (cipherShift < 0) cipherShift = 26 + (cipherShift % 25);
+        // if character is lowerCase
+        if (stringToEncrypt.charCodeAt(index) >= 97 && stringToEncrypt.charCodeAt(index) <= 122) {
+            encryptedString += String.fromCharCode((stringToEncrypt.charCodeAt(index) - 97 + cipherShift) % 26 + 97);
+        } else if (stringToEncrypt.charCodeAt(index) >= 65 && stringToEncrypt.charCodeAt(index) <= 90) { // if character is UpperCase
+            encryptedString += String.fromCharCode((stringToEncrypt.charCodeAt(index) - 65 + cipherShift) % 26 + 65);
+        } else { // every other character
+            encryptedString += String.fromCharCode(stringToEncrypt.charCodeAt(index));
+        }
+    }
+    return encryptedString
 }
 
 module.exports = caesar
