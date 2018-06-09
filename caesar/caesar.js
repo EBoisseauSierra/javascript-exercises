83ba753db971e2be9a96b90679e6011e63510184
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-09 11:42:56 -0400

    Handle less than -26 cipherShift cornercase

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,8 +1,8 @@
-var caesar = function(stringToEncrypt, cipherShift) {
+let caesar = function(stringToEncrypt, cipherShift) {
     let encryptedString = '';
     for (let index = 0; index < stringToEncrypt.length; index++) {
         // transform negative shift into positive one, as it otherwise messes with wraping
-        if (cipherShift < 0) cipherShift = 26 + (cipherShift % 25);
+        if (cipherShift < 0) cipherShift = 26 + (cipherShift % 26);
         // if character is lowerCase
         if (stringToEncrypt.charCodeAt(index) >= 97 && stringToEncrypt.charCodeAt(index) <= 122) {
             encryptedString += String.fromCharCode((stringToEncrypt.charCodeAt(index) - 97 + cipherShift) % 26 + 97);
