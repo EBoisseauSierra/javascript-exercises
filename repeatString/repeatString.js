71ca52d71dc7f580451b43238bafa5747a8b9086
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-08 20:56:56 -0400

    Add solution to repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-var repeatString = function() {
-
+var repeatString = function(stringToRepeat, numberOfRepetition) {
+    if (numberOfRepetition < 0) return 'ERROR';
+    let stringRepeated = '';
+    for (let i = 0; i < numberOfRepetition; i++) {
+        stringRepeated += stringToRepeat;
+    }
+    return stringRepeated;
 }
 
 module.exports = repeatString
