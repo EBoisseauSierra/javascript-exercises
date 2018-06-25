692e37b4eb1a7202e0c40d2321a03fef1db3cd5c
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-25 18:15:46 -0400

    Add solution to pigLatin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,23 @@
-function translate() {
-	// body...
+function translate(sentence) {
+	return sentence.split(' ').map(word => pigLatin(word)).join(' ');
+}
+
+function pigLatin(word) {
+	// word beginning with a vowel
+	if(/^[aeiou]/.test(word)) return word + 'ay';
+	// word beginning with three consonant
+	if(/^[bcdfghjklmnpqrstvwxz]{3}/.test(word)) return word.slice(3) + word.slice(0,3) + 'ay';
+	// word beginning with consonant then 'qu'
+	if(/^[bcdfghjklmnpqrstvwxz]qu/.test(word)) return word.slice(3) + word.slice(0,3) + 'ay';
+	// word beginning with two consonant
+	if(/^[bcdfghjklmnpqrstvwxz]{2}/.test(word)) return word.slice(2) + word.slice(0,2) + 'ay';
+	// word beginning with 'qu'
+	if(word.slice(0, 2) == 'qu') return word.slice(2) + 'quay'
+	// word beginning with a consonant
+	if(/^[bcdfghjklmnpqrstvwxz]/.test(word)) return word.slice(1) + word.slice(0,1) + 'ay';
 }
 
 
 module.exports = {
 	translate
 }
-
