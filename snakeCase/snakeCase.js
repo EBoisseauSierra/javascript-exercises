fddeb89a5900e21aac208320f54554d974cf7fac
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-25 17:47:47 -0400

    Add partial solution to snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,21 @@
-var snakeCase = function() {
+let snakeCase = function(text) {
+    arr = text.split('').map(char => {
+        // console.log('> '+char);
+        if (/[a-zA-z]/.test(char)) {
+            return char.toLowerCase();
+        }
+        if (/[ -]/.test(char)){
+            return '_'
+        }
+        return '';
+    })
+    return arr.join('');
+}
 
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
 }
 
 module.exports = snakeCase
