50a11c2f24d8136125cf83213f19ecf9a34ebbd0
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-25 17:14:54 -0400

    Add solution to palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-var palindromes = function() {
-
+var palindromes = function(textToTest) {
+    arrayToTest = textToTest.toLowerCase().split('').filter(character =>{
+        if (character.charCodeAt(0) >= "a".charCodeAt(0) && character.charCodeAt(0) <= "z".charCodeAt(0)) return true;
+    });
+    for (let i=0, len = arrayToTest.length, half = Math.floor(len / 2) + 1; i < half; i++){
+        if (arrayToTest[i] !== arrayToTest[len-(i+1)]) return false;
+    }
+    return true;
 }
 
 module.exports = palindromes
